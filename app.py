99d7a6845cc5d8a6dea36c95ca5d1efb07afb2a6
charan <sravanamcharan20@gmail.com>
2025-04-08 15:07:31 +0530

    deployment changes

--- app.py
+++ app.py
@@ -50,4 +50,4 @@ def predict():
 # Updated port binding for Render
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
-    app.run(debug=True, host="0.0.0.0", port=port)
+    app.run(debug=False, host="0.0.0.0", port=port)
